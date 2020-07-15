7bff708b8ce178ce4ce4c4a9f9ea22657c7c75eb
anettkal <anett.kalamar@gmail.com>
2020-07-15 13:05:28 +0200

    exercises with variables

---
+++ JS_Stalker_Exercise/script.js
@@ -0,0 +1,8 @@
+var firstName = prompt ("What's your first name?");
+var lastName = prompt ("what's your last name?");
+var age = prompt ("What's your age?");
+
+var fullName = firstName + " " + lastName
+
+console.log("Your full name is " + fullName);
+console.log("Your are " + userAge + " years old");
