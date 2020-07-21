6c4055abaac546f5b0e2223d904cf1c5a69c949e
anettkal <anett.kalamar@gmail.com>
2020-07-21 15:31:02 +0200

    Added: * forEach function added for list * "stars added console.log" * todos.push added for new todo * delete todo added (+ todos.splice)

--- TodoList/list.js
+++ TodoList/list.js
@@ -6,15 +6,27 @@ window.setTimeout(function() {
 var input = prompt("What would you like to do?")
 
 while (input !== "quit") {
-    if(input === "list") {
     //handle input
-      console.log(todos);
+  if(input === "list") {
+      console.log("**********")
+      todos.forEach(function(todo, i){
+          console.log(i + ": " + todo);
+      });
+      console.log("**********")
     } else if (input === "new") {
       //ask for new todo
       var newTodo = prompt ("Enter new todo");
       //add to todos array
       todos.push(newTodo);
-     }
+      console.log("Added Todo");
+    } else if(input === "delete") {
+      //ask for index of todo to be deleted
+      var index = prompt ("Enter index of todo to delete")
+      //delete that todo
+      //splice()
+      todos.splice(index,1);
+      console.log("Deleted Todo");
+    }
     //ask again for new input
      input = prompt("What would you like to do?")
 }
